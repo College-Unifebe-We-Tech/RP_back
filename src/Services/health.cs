aad9d9b8ecb9d0fda787b7809a72ff07d5da811e
rubensantoniorosa2704 <rubensrosaneto27@gmail.com>
2024-05-02 23:34:08 -0300

    fix health route

--- src/Services/health.cs
+++ src/Services/health.cs
@@ -1,13 +1,14 @@
 public class HealthService {
-    private readonly IRepositoryHealth<Health> _repository; // interface to whatever will be the source of data.
+    private readonly IRepositoryHealth<Health> _repository;
 
     public HealthService() {
-        _repository = new HealthRepository(); // in this case it will be this one using sql server.
+        _repository = new HealthRepository();
     }
 
-    public async Task<string> Check() {
+    // Change the return type to Task<Health>
+    public async Task<Health> Check() {
         Health? createdHealth = await _repository.Create(DateTime.Now);
-                
-        return createdHealth.sync.ToLongDateString();
+        // Ensure that createdHealth is not null before accessing its properties
+        return createdHealth;
     }
 }

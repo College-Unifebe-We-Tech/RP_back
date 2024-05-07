0d620df508bdf5955e3f77f4ee156c04b8c8193d
Anthony Muller <anthonyvmuller@outlook.com>
2024-05-07 18:55:02 -0300

    build: add five default categories on migration

---
+++ Migrations/20240507214311_categories.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RP_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class categories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Category (CategoryName) VALUES ('calçado')");
+            migrationBuilder.Sql("INSERT INTO Category (CategoryName) VALUES ('bolsa')");
+            migrationBuilder.Sql("INSERT INTO Category (CategoryName) VALUES ('camisa')");
+            migrationBuilder.Sql("INSERT INTO Category (CategoryName) VALUES ('garrafa')");
+            migrationBuilder.Sql("INSERT INTO Category (CategoryName) VALUES ('mouse')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
